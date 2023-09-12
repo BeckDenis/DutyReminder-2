23add72a7b816f33cef6b36996e5e9f908af105d
denis <denisbeck555@gmail.com>
2023-09-13 02:56:47 +0600

    Add Fragments

--- app/src/main/java/denis/beck/dutyreminder_2/fragments/main/MainFragment.kt
+++ app/src/main/java/denis/beck/dutyreminder_2/fragments/main/MainFragment.kt
@@ -6,23 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import denis.beck.dutyreminder_2.R
+import denis.beck.dutyreminder_2.databinding.FragmentMainBinding
 import denis.beck.dutyreminder_2.remindManager.RemindManager
 
 class MainFragment : Fragment() {
 
-    private lateinit var remindManager : RemindManager
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var remindManager: RemindManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.timePickerButton.setOnClickListener {
+
+        }
+        binding.datePickerButton.setOnClickListener {
+
+        }
+
         remindManager = RemindManager(requireContext())
         remindManager.setReminder()
     }
-
 }
---
+++ app/src/main/java/denis/beck/dutyreminder_2/fragments/pickers/date/DatePickerDialog.kt
@@ -0,0 +1,26 @@
+package denis.beck.dutyreminder_2.fragments.pickers.date
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+
+class DatePickerDialog : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current time as the default values for the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of TimePickerDialog and return it
+        return DatePickerDialog(requireContext(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        TODO("Not yet implemented")
+    }
+}
