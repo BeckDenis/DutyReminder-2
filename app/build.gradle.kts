b6fa43c1342fb859726dcd75d598395101205097
denis <denisbeck555@gmail.com>
2023-09-13 21:04:16 +0600

    Add room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -45,12 +46,16 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.6.2")
+
+    implementation("androidx.room:room-runtime:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
